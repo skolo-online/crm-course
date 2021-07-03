ea5554c5ce0e0966e90821508d765101c394f4e7
Bertha <bertha.kgokong@tatisoftware.com>
2021-07-03 10:51:31 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import config
+
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+
+app = Flask(__name__)
+app.config.from_object(config.config['development'])
+
+app.register_error_handler(404, page_not_found)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', **locals())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8888', debug=True)
